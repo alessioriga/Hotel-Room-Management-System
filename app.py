a9106caea8c933cfb4d6c9f58c0bc1012819cb2c
Alessio Rigamonti <alessioriga93@hotmail.it>
2026-03-04 19:11:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, redirect
+from auth.login import login_bp
+from routes.home import home_bp
+
 
 app = Flask(__name__)
 app.secret_key = "ucenmanchester2026"
+
+app.register_blueprint(login_bp)
+app.register_blueprint(home_bp)
 
 @app.route("/")
 def start():
