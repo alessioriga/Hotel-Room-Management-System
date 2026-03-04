8169fe40c8edc2e8f9c3c78af8e8292d0a95f473
Alessio Rigamonti <alessioriga93@hotmail.it>
2026-03-04 19:03:07 +0000

    Update decorators.py

--- auth/decorators.py
+++ auth/decorators.py
@@ -2,3 +2,13 @@ from functools import wraps
 from flask import session, redirect, url_for
 
 
+def login_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+
+        if 'username' not in session:
+            return redirect(url_for('login_bp.login'))
+        
+        return f(*args, **kwargs)
+    
+    return decorated
