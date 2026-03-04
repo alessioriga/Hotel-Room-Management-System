de85b2390d3db70cd420b83d90dca7ba077598a1
Alessio Rigamonti <alessioriga93@hotmail.it>
2026-03-04 19:04:41 +0000

    Update login.py

--- auth/login.py
+++ auth/login.py
@@ -4,3 +4,28 @@ from db.connection import login_db_connection
 
 login_bp = Blueprint('login_bp', __name__)
 
+@login_bp.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        connection = login_db_connection()
+        user = connection.execute('SELECT * FROM Users WHERE username = ? AND password = ?', (username, password)).fetchone()
+        connection.close()
+
+        if user:
+            session['username'] = user['username']
+            session['account_name'] = user['account_name']
+            return redirect (url_for('home_bp.home')) 
+        else:
+            return render_template('login.html', error="Incorrect username or password")
+ 
+    # When the user first opens /login in the browser, they are doing a GET request.
+    # request.method == 'POST' is False, because they haven't submitted the form yet.
+    return render_template('login.html')
+
+@login_bp.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login_bp.login'))
